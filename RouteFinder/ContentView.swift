daf455e40b457a267677b0893c7a1f8d78e5a536
Dennis <Dennisb723@gmail.com>
2026-05-14 21:24:57 -0500

    Finished view for test data

--- RouteFinder/ContentView.swift
+++ RouteFinder/ContentView.swift
@@ -8,28 +8,34 @@
 import SwiftUI
 
 
+struct Route: Codable {
+    let routeDistanceM: Int
+    let routeDuractionSec: Int
+}
+
+
 enum TransitOption: String, CaseIterable, Identifiable {
-    case driving
-    case walking
-    case cycling
+    case driving = "driving"
+    case walking = "walking"
+    case cycling = "cycling"
     
     var id: String { self.rawValue }
     
 }
 
 enum OptimizationOption: String, CaseIterable, Identifiable {
-    case time
-    case distance
+    case time = "time"
+    case distance = "distance"
     
     var id: String { self.rawValue }
 }
 
 struct ContentView: View {
     
-//    @State var originLat: Float
-//    @State var originLon: Float
-//    @State var destinationLat: Float
-//    @State var destinationLon: Float
+    @State var originLat: Double = 0.0
+    @State var originLon: Double = 0.0
+    @State var destinationLat: Double = 0.0
+    @State var destinationLon: Double =  0.0
     
     @State var selectedTransit: TransitOption = .driving
     @State var selectedOptimitzation: OptimizationOption = .time
@@ -38,28 +44,121 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("Transit Option: ")
-            Picker("Transit Option", selection: $selectedTransit) {
-                ForEach(TransitOption.allCases) { transitOption in
-                    Text(transitOption.rawValue.capitalized).tag(transitOption)
-                }
+            
+            Text("Find a Route")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            VStack() {
+                Text("Origin Latitude: ")
+                TextField("Origin Latitude", value: $originLat, format: .number)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                    
+                    
             }
-            Text("Optimization Mode:")
-            Picker("Optimization Mode", selection: $selectedOptimitzation) {
-                ForEach(OptimizationOption.allCases) {optimizationOption in
-                    Text(optimizationOption.rawValue.capitalized).tag(optimizationOption)
+            
+            VStack {
+                Text("Origin Longitude: ")
+                TextField("Origin Longitude", value: $originLon, format: .number)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+            }
+            
+            VStack() {
+                Text("Destination Latitude: ")
+                TextField("Destination Latitude", value: $destinationLat, format: .number)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+            }
+            
+            VStack {
+                Text("Destination Longitude: ")
+                TextField("Destination Longitude", value: $destinationLon, format: .number)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                    .frame(height: 50)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+            }
+            
+            HStack {
+                Text("Transit Option: ")
+                Spacer()
+                Picker("Transit Option", selection: $selectedTransit) {
+                    ForEach(TransitOption.allCases) { transitOption in
+                        Text(transitOption.rawValue.capitalized).tag(transitOption)
+                    }
                 }
+                .padding(.horizontal)
+                .frame(height: 50)
+                .frame(maxWidth: 200)
+                .background(Color(UIColor.secondarySystemBackground))
+                .cornerRadius(10)
             }
 
-
+            HStack {
+                Text("Optimization Mode:")
+                Spacer()
+                Picker("Optimization Mode", selection: $selectedOptimitzation) {
+                    ForEach(OptimizationOption.allCases) {optimizationOption in
+                        Text(optimizationOption.rawValue.capitalized).tag(optimizationOption)
+                    }
+                    
+                }
+                .padding(.horizontal)
+                .frame(height: 50)
+                .frame(maxWidth: 200)
+                .background(Color(UIColor.secondarySystemBackground))
+                .cornerRadius(10)
+            }
+            
+            Spacer()
+            
+            Button("Find Route") {
+                Task {
+                    do {
+                        try await findRoute()
+                    } catch {
+                        print("Unsuccessful")
+                    }
+                }
+            }
+            .foregroundColor(Color.white)
+            .font(.headline)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .cornerRadius(10)
+            .padding()
         }
         .padding()
     }
-}
-
-struct Route: Codable {
-    let routeDistanceM: Int
-    let routeDuractionSec: Int
+    
+    func findRoute() async throws -> () {
+        print($originLat)
+        print($originLon)
+        print($destinationLat)
+        print($destinationLon)
+        print($selectedTransit)
+        print($selectedOptimitzation)
+        
+        return
+    }
 }
 
 #Preview {
